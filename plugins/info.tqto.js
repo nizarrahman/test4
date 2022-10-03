7f4d43804d2aac0b96820d3a7b408bfabb1092b8
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2022-10-03 10:28:02 +0700

    Create info.tqto.js

---
+++ plugins/info.tqto.js
@@ -0,0 +1,44 @@
+import fetch from 'node-fetch'
+
+let handler = async (m) => {
+    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+    //let wibu = `https://api.zacros.my.id/randomimg/loli`
+    let res = await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/neko.txt')
+    let txt = await res.text()
+    let arr = txt.split('\n')
+    let cita = arr[Math.floor(Math.random() * arr.length)]
+    let thumb = await(await fetch(cita)).buffer()
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    else who = m.sender
+    let user = global.db.data.users[who]
+let tqto = `*▸ - - - —「 BIG THANKS TO 」— - - - ◂*
+*❉ Allah SWT*
+*❉ Nabi Muhammad Saw*
+*❉ My Team:*
+
+
+*▸ - - - —「 Thanks For Team 」— - - - ◂*
+*❉ Nizar Rahman:*https://instagram.com/nizarrahman.f_
+*❉ Irham:* https://instagram.com/yuki_sen23
+*❉ Adyran:* https://instagram.com/adrynngrha
+*▸ - - - —「 Thanks For Support system 」— - - - ◂*
+*❉ Ortu Gw*
+*❉ All Friend*
+
+`
+conn.sendButtonDoc(m.chat, tqto, wm,'Thanks','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/nizarrrahman.f_",
+    mediaType: "VIDEO",
+    description: "https://Instagram.com/nizarrrahman.f_", 
+    title: 'WaBot-MultiDevice-By Nizar',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
+}
+} })
+        }
+handler.help = ['tqto']
+handler.tags = ['main','info']
+handler.command = /^(credits|credit|thanks|thanksto|tqto)$/i
+export default handler
